888c15bceaac35f135abbe57d171358f69dc0045
Kenzy Louis <kenzylouis@gmail.com>
2017-08-26 12:56:18 -0500

    refactoring app.js appTest and added another test

--- test/appTest.js
+++ test/appTest.js
@@ -4,7 +4,13 @@ const sayHello = require('../app').sayHello;
 //const app = require('../app');
 
 describe('App', function(){
-  it('app should return hello', function(){
-    assert.equal(sayHello(), 'hello');
+  it('sayHello should return hello', function(){
+    let result = sayHello();
+    assert.equal(result, 'hello');
+  });
+
+  it('sayHello should return type string', function(){
+    let result = sayHello();
+    assert.typeOf(result, 'string');
   });
 });
