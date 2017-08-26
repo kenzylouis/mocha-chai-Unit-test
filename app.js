b89f71624fc839ff1b79d247c4729092c928517a
Kenzy Louis <kenzylouis@gmail.com>
2017-08-26 12:43:33 -0500

    assert hello

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
-module.export - function(){
-  return 'Hello';
+module.exports = function(){ // whatever we put here will be accessible outside of the file
+  return 'hello';
 }
--- test/appTest.js
+++ test/appTest.js
@@ -1,9 +1,9 @@
-// const assert = require('assert');  // if you want to use the default aassertion library from node
-const assert = require('assert').assert;
+// const assert = require('assert');  // if you want to use the default aassertion library from node.js
+const assert = require('chai').assert;
 const app = require('../app');
 
 describe('App', function(){
-  it('app shoould return hello', function(){
+  it('app should return hello', function(){
     assert.equal(app(), 'hello');
   });
 });
