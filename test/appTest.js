61dce013b028274901d8c754baa2e0edcd4b23e2
Kenzy Louis <kenzylouis@gmail.com>
2017-08-26 13:35:00 -0500

    refactoring appTest to load full app.js

--- test/appTest.js
+++ test/appTest.js
@@ -1,27 +1,27 @@
 // const assert = require('assert');  // if you want to use the default aassertion library from node.js
 const assert = require('chai').assert;
-const sayHello = require('../app').sayHello;
-const addNumbers = require('../app').addNumbers;
-//const app = require('../app');
+//const sayHello = require('../app').sayHello;
+//const addNumbers = require('../app').addNumbers;
+const app = require('../app');
 
 describe('App', function(){
   it('sayHello should return hello', function(){
-    let result = sayHello();
+    let result = app.sayHello();
     assert.equal(result, 'hello');
   });
 
   it('sayHello should return type string', function(){
-    let result = sayHello();
+    let result = app.sayHello();
     assert.typeOf(result, 'string');
   });
 
   it('addNumbers should be above 5', function(){
-    let result = addNumbers(5,5);
+    let result = app.addNumbers(5,5);
     assert.isAbove(result, 5);
   });
 
   it('addNumbers should return type number', function(){
-    let result = addNumbers(5,5);
+    let result = app.addNumbers(5,5);
     assert.typeOf(result, 'number');
   });
 });
